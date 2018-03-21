ea6d83fff84fa6696d76a81b0357e867e37a0fb0
Anderson Fernandes <handerson171@gmail.com>
2018-03-21 15:09:08 -0500

    Fixed minor bugs in sale show view

--- resources/views/partial/sales/_show.blade.php
+++ resources/views/partial/sales/_show.blade.php
@@ -203,7 +203,7 @@
             {{ Date::parse($event->start)->format('l, F j, Y \a\t g:i A') }}
             <div class="ui black circular label">{{ $event->type->name }}</div>
           </div>
-          <a href="{{ route('admin.events.edit', $event) }}" target="_blank">{{ $event->show->name }}</a>
+          <a href="{{ route('admin.events.show', $event) }}" target="_blank">{{ $event->show->name }}</a>
           <div class="sub header">
             @foreach($sale->tickets->unique('ticket_type_id') as $ticket)
               <div class="ui black label" style="margin-left:0">
