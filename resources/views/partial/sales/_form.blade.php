0b40e0cbad4b13e66524537bb364d449bfb174f2
Anderson Fernandes <handerson171@gmail.com>
2018-08-22 15:19:14 -0500

    Trying another fix for events dropdown in sale edit

--- resources/views/partial/sales/_form.blade.php
+++ resources/views/partial/sales/_form.blade.php
@@ -898,19 +898,26 @@
       @else
         $("#customers").dropdown('set selected', {{ old('customer_id') }})
       @endif
+      console.log('document is ready')
+      @isset($sale)
+
+        setTimeout(function() {
+          @if ($sale->events->count() > 0)
+            @foreach ($sale->events as $event)
+              $($('.date')[{{ $loop->index }}]).trigger('change')
+              $($('.ui.search.selection.dropdown').not('#customers')[{{ $loop->index }}]).dropdown('set selected', {{ $event->id }})
+            @endforeach
+          @endif
+          console.log('setting events in their dropdowns')
+        }, 500)
+
+
+      @endisset
     }, 500)
+
   })
 
-  @isset($sale)
-    $(window).load(function() {
-      @if ($sale->events->count() > 0)
-        @foreach ($sale->events as $event)
-          $($('.date')[{{ $loop->index }}]).trigger('change')
-          $($('.ui.search.selection.dropdown').not('#customers')[{{ $loop->index }}]).dropdown('set selected', {{ $event->id }})
-        @endforeach
-      @endif
-    })
-  @endisset
+
 
   {{-- Change Sale Status color whenever the sale status changes --}}
   $('#status').change(changeSaleStatusColor)
