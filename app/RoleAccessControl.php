2d53ce7e4eaf8ad694ab8a4ae3032d3c3c990178
Anderson Fernandes <handerson171@gmail.com>
2018-10-04 16:39:20 -0500

    Fixed creating role bug, new role permissions

--- app/RoleAccessControl.php
+++ app/RoleAccessControl.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoleAccessControl extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'dashboard', 'shows', 'products', 'calendar', 'sales', 'reports',
+        'members', 'users', 'organizations', 'bulletin', 'settings',
+        'admin', 'cashier'
+    ];
+
     protected $table = 'roles_access_control';
 }
