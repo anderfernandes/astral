f43e6f4d292d0a7cf9e01a2493a66cc03b69db13
Anderson Fernandes <handerson171@gmail.com>
2019-07-03 15:52:32 -0500

    Testing simple HTML email template

--- resources/views/email/new-schedule.blade.php
+++ resources/views/email/new-schedule.blade.php
@@ -1,146 +1,69 @@
-@extends('layout.email-semantic')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+<body>
 
-<style>
-  table, th, td { border: 1px solid; }
-</style>
+  <style>
+    table { width: 100%;}
+    td, th { border: 1px solid black}
+    td { text-align: center }
+  </style>
 
-<h4 class="ui header">
-  {{ now()->format('l, F j, Y') }}
-</h4>
+  <h4 class="ui header">
+      {{ now()->format('l, F j, Y') }}
+    </h4>
+    
+    <p>Dear {{ $user->firstname }},</p>
+    
+    @if (isset($schedule->memo))
+      <p>{{ $schedule->memo }}</p>
+    @else
+      <p>A new work schedule has been posted:</p>
+    @endif
+    
+    <p>Note: This schedule is not final, as it may change to accommodate new, updated or canceled events.</p>
 
-<p>Dear {{ $user->firstname }},</p>
-
-<br>
-
-@if (isset($schedule->memo))
-  <p>{{ $schedule->memo }}</p>
-@else
-  <p>A new work schedule has been posted:</p>
-@endif
-
-<br>
-
-<p>Note: This schedule is not final, as it may change to accommodate new, updated or canceled events.</p>
-
-<br>
-
-<div class="ui three raised doubling link cards">
     @foreach ($schedule->shifts as $shift)
-    <div class="card">
-      <div class="content">
-        <a href="{{ route('admin.shifts.show', $shift) }}" class="header">
-          #{{ $shift->id }} - {{ $shift->start->format('l, F j, Y') }}
-        </a>
-        <div class="meta">
-          <i class="clock icon"></i>
-          {{ $shift->start->format('g:i A') }} - {{ $shift->end->format('g:i A') }}
-          ({{ $shift->end->diffInMinutes($shift->start) / 60 }} hours)
-        </div>
-        <div class="description">
-          <div class="ui sub header">Staff</div>
-          @foreach ($shift->employees as $employee) 
-          <div class="ui fluid black label" style="margin-bottom:0.5rem">
-            <i class="user circle icon"></i>
-            {{ $employee->firstname }}
-            <div class="detail">{{ $shift->positions[$loop->index]->name }}</div>
-          </div>
-          @endforeach
-          <div class="ui comments">
-            <div class="ui sub header">Events</div>
-            @foreach ($shift->events as $event)
-              <div class="comment">
-                
-                <div class="content">
-                  <div class="author">
-                    {{ $event->show->name }}
-                    <div class="ui tiny black basic label">{{ $event->show->type }}</div>
-                  </div>
-                  <div class="metadata">
-                    <span class="date">
-                      {{ $event->start->format('g:i A') }} | 
-                      {{ $event->type->name }} |
-                      {{ $event->tickets->count() }} 
-                      {{ $event->tickets->count() == 1 ? "seat" : "seats" }} reserved |
-                      {{ $event->sales->count() }} {{ $event->sales->count() == 1 ? "sale" : "sales" }}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            @endforeach
-          </div>
-        </div>
-      </div>
-    </div>
-    @endforeach
-  </div>
-
-<!--<table style="border: 1px solid">
-  <thead>
-    <tr>
-      <th colspan="{{ $schedule->shifts->count() * 2 }}">
-        Schedule #{{ $schedule->id }} | Created by {{ $schedule->creator->firstname }}
-      </th>
-    </tr>
-    <tr>
-      @foreach ($schedule->shifts as $shift)
-        <th colspan="2">{{ $shift->start->format('l, F j') }}</th>
-      @endforeach
-    </tr>
-    <tr>
-      @foreach ($schedule->shifts as $shift)
+    <table>
+      <tr>
+        <th colspan="3">{{ $shift->start->format('l, F j, Y') }}</th>
+      </tr>
+      <tr>
         <th>Employee</th>
+        <th>Position</th>
         <th>Time</th>
+      </tr>
+      @foreach ($shift->employees as $employee)
+      <tr <?php if ($employee->id == $user->id) echo 'style="background-color:lightgray"'; ?>>
+        <td>{{ $employee->firstname }}</td>
+        <td>{{ $shift->positions[$loop->index]->name }}</td>
+        <td>{{ $shift->start->format('g:i A') }} - {{ $shift->end->format('g:i A') }}</td>
+      </tr>
       @endforeach
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      @foreach ($schedule->shifts as $shift)
-      <td colspan="2">
-        <table style="border: 0">
-          @foreach ($shift->employees as $employee)
-          <tr>
-            <td style="border: 0; text-align: center">
-              {{ $employee->firstname }} <br>
-              {{ $shift->positions[$loop->index]->name }}
-            </td>
-            <td style="border: 0; text-align: center">
-              {{ $shift->start->format('g:i A') }} - {{ $shift->end->format('g:i A') }}
-            </td>
-          </tr>
-          @endforeach
-        </table>
-      </td>
+      @if ($shift->events->count() > 0)
+      <tr>
+        <th colspan="3">Events</th>
+      </tr>
+      @foreach ($shift->events as $event)
+      <tr>
+        <td colspan="3">
+          #{{ $event->id }} - {{ $event->show->name }}, {{ $event->show->type }}
+          {{ $event->start->format('g:i A') }}, {{ $event->type->name }}, 
+          {{ $event->tickets->count() }} {{ $event->tickets->count() == 1 ? "ticket" : "tickets" }} sold,
+          {{ $event->tickets->count() }} {{ $event->tickets->count() == 1 ? "sale" : "sales" }}
+        </td>
+      </tr>
       @endforeach
-    </tr>
-    <tr>
-      @foreach ($schedule->shifts as $shift)
-        @if ($shift->events->count() > 0)
-        <th colspan="2">Events</th>
-        @endif
-      @endforeach
-    </tr>
-    <tr>
-      @foreach ($schedule->shifts as $shift)
-      <td colspan="2" style="text-align:center">
-        @foreach ($shift->events as $event)
-      <p>#{{ $event->id }} - {{ $event->show->name }}, {{ $event->start->format('g:i A') }}</p>
-        @endforeach
-      </td>
-      @endforeach
-    </tr>
-  </tbody>
-</table>-->
-
-<br><br>
-
-<p>
-  {{ auth()->user()->fullname }}     <br />
-  {{ auth()->user()->role->name }}   <br />
-  {{ auth()->user()->organization->name }} <br />
-</p>
-<p></p>
-
-@endsection
+      @endif
+    </table>
+    <br>
+    @endforeach
+    
+    <p>
+      {{ auth()->user()->fullname }}     <br />
+      {{ auth()->user()->role->name }}   <br />
+      {{ auth()->user()->organization->name }} <br />
+    </p>
+    <p></p>
+</body>
+</html>
