4c46c3d9837ff95bbe3c3ea1801abf046f4e7402
Anderson Fernandes <handerson171@gmail.com>
2019-11-07 14:55:12 -0600

    Fixed validation rules for trailers and expiration dates in shows

--- app/Http/Controllers/Admin/ShowController.php
+++ app/Http/Controllers/Admin/ShowController.php
@@ -2,241 +2,230 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\{ Show, ShowType };
+use App\{Show, ShowType};
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use Session;
-use Illuminate\Support\Facades\{ Auth, Log, Storage };
+use Illuminate\Support\Facades\{Auth, Log, Storage};
 
 class ShowController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        //$shows = Show::where('id', '<>', 1)->orderBy('name', 'asc')->paginate(10);
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    //$shows = Show::where('id', '<>', 1)->orderBy('name', 'asc')->paginate(10);
 
-        // Filtering non-empty query strings
+    // Filtering non-empty query strings
 
 
-        $shows = Show::where('id', '!=', 1);
-        $showTypes = ShowType::where('id', '!=', 1)->orderBy('name', 'asc')->pluck('name', 'id');
+    $shows = Show::where('id', '!=', 1);
+    $showTypes = ShowType::where('id', '!=', 1)->orderBy('name', 'asc')->pluck('name', 'id');
 
-        if (count($request->query()) > 0)
-        {
-          // In the future, change this to $request->has
-          if (isSet($request->id)) {
-            $shows = $shows->where('id', $request->id);
-          }
+    if (count($request->query()) > 0) {
+      // In the future, change this to $request->has
+      if (isset($request->id)) {
+        $shows = $shows->where('id', $request->id);
+      }
 
-          if (isSet($request->duration)) {
-            $shows = $shows->where('duration', $request->duration);
-          }
+      if (isset($request->duration)) {
+        $shows = $shows->where('duration', $request->duration);
+      }
 
-          if (isSet($request->type_id)) {
-            $shows = $shows->where('type_id', $request->type_id);
-          }
+      if (isset($request->type_id)) {
+        $shows = $shows->where('type_id', $request->type_id);
+      }
 
-          if ($request->has('active')) {
-            $shows = $shows->where('active', (bool)$request->active);
-          }
+      if ($request->has('active')) {
+        $shows = $shows->where('active', (bool) $request->active);
+      }
 
-          $showIds = $shows->pluck('id');
-          $shows = Show::whereIn('id', $showIds)->orderBy('name', 'asc')->paginate(10);
-        }
-        else
-        {
-          $shows = $shows->orderBy('name', 'asc')->paginate(10);
-        }
-
-        // if app.force_https is true, make pagination links have https in them
-
-        if (config('app.force_https'))
-        {
-          $shows->setPath('/admin/shows');
-        }
-
-        return view('admin.shows.index')->withShowTypes($showTypes)
-                                        ->withShows($shows);
+      $showIds = $shows->pluck('id');
+      $shows = Show::whereIn('id', $showIds)->orderBy('name', 'asc')->paginate(10);
+    } else {
+      $shows = $shows->orderBy('name', 'asc')->paginate(10);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $showTypes = ShowType::where('id', '!=', 1)->orderBy('name', 'asc')->pluck('name', 'id');
-        return view('admin.shows.create')->withShowTypes($showTypes);
+    // if app.force_https is true, make pagination links have https in them
+
+    if (config('app.force_https')) {
+      $shows->setPath('/admin/shows');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-          'name'        => 'required|unique:shows',
-          'description' => 'required',
-          'type_id'     => 'required',
-          'duration'    => 'required|integer',
-          'cover'       => 'image',
-          'trailer_url' => 'url',
-          'expiration'  => 'date',
-        ]);
+    return view('admin.shows.index')->withShowTypes($showTypes)
+      ->withShows($shows);
+  }
 
-        $show = new Show;
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    $showTypes = ShowType::where('id', '!=', 1)->orderBy('name', 'asc')->pluck('name', 'id');
+    return view('admin.shows.create')->withShowTypes($showTypes);
+  }
 
-        $show->name        = $request->name;
-        $show->description = $request->description;
-        $show->type        = ShowType::find($request->type_id)->name;
-        $show->type_id     = $request->type_id;
-        $show->duration    = $request->duration;
-        $show->active      = (bool)$request->active;
-        $show->trailer_url = $request->trailer_url ?? null;
-        $show->expiration  = $request->expiration ?? null;
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $this->validate($request, [
+      'name'        => 'required|unique:shows',
+      'description' => 'required',
+      'type_id'     => 'required',
+      'duration'    => 'required|integer',
+      'cover'       => 'image',
+      'trailer_url' => 'nullable|url',
+      'expiration'  => 'nullable|date',
+    ]);
 
-        $show->cover = $request->cover == null ? '/default.png' : $request->cover->store('shows', 'public');
+    $show = new Show;
 
-        $show->creator_id  = Auth::user()->id;
+    $show->name        = $request->name;
+    $show->description = $request->description;
+    $show->type        = ShowType::find($request->type_id)->name;
+    $show->type_id     = $request->type_id;
+    $show->duration    = $request->duration;
+    $show->active      = (bool) $request->active;
+    $show->trailer_url = $request->trailer_url ?? null;
+    $show->expiration  = $request->expiration ?? null;
 
-        $show->save();
+    $show->cover = $request->cover == null ? '/default.png' : $request->cover->store('shows', 'public');
 
-        Session::flash('success',
-          "The <strong>{$show->type}</strong> show <strong>{$show->name}</strong> has been added successfully!"
-        );
+    $show->creator_id  = Auth::user()->id;
 
-        // Log created event
-        Log::info(Auth::user()->fullname . ' created Show ' . $show->name .' using admin');
+    $show->save();
 
-        return redirect()->route('admin.shows.show', $show);
-    }
+    Session::flash(
+      'success',
+      "The <strong>{$show->type}</strong> show <strong>{$show->name}</strong> has been added successfully!"
+    );
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Show  $show
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Show $show)
-    {
-        $showTypes = ShowType::where('id', '!=', 1)->orderBy('name', 'asc')->pluck('name', 'id');
+    // Log created event
+    Log::info(Auth::user()->fullname . ' created Show ' . $show->name . ' using admin');
 
-        return view('admin.shows.show')->with(['showTypes' => $showTypes])
-                                       ->withShow($show);
-    }
+    return redirect()->route('admin.shows.show', $show);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Show  $show
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Show $show)
-    {
-        return view('admin.shows.edit')->withShow($show);
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Show  $show
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Show $show)
+  {
+    $showTypes = ShowType::where('id', '!=', 1)->orderBy('name', 'asc')->pluck('name', 'id');
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Show  $show
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Show $show)
-    {
+    return view('admin.shows.show')->with(['showTypes' => $showTypes])
+      ->withShow($show);
+  }
 
-      $this->validate($request, [
-        'name'        => 'required',
-        'description' => 'required',
-        'type_id'     => 'required',
-        'duration'    => 'required|integer',
-        'trailer_url' => 'nullable|url',
-        'expiration'  => 'nullable|date',
-      ]);
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Show  $show
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Show $show)
+  {
+    return view('admin.shows.edit')->withShow($show);
+  }
 
-      $show->name        = $request->input('name');
-      $show->description = $request->input('description');
-      $show->type        = ShowType::find($request->type_id)->name;
-      $show->type_id     = $request->type_id;
-      $show->duration    = $request->input('duration');
-      $show->active      = (bool)$request->active;
-      $show->trailer_url = $request->trailer_url ?? null;
-      $show->expiration  = $request->expiration ?? null;
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Show  $show
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Show $show)
+  {
 
-      $request->has('cover') ? $show->cover =  $request->cover->store('shows', 'public') : null;
+    $this->validate($request, [
+      'name'        => 'required',
+      'description' => 'required',
+      'type_id'     => 'required',
+      'duration'    => 'required|integer',
+      'trailer_url' => 'nullable|url',
+      'expiration'  => 'nullable|date',
+    ]);
 
-      $show->save();
+    $show->name        = $request->input('name');
+    $show->description = $request->input('description');
+    $show->type        = ShowType::find($request->type_id)->name;
+    $show->type_id     = $request->type_id;
+    $show->duration    = $request->input('duration');
+    $show->active      = (bool) $request->active;
+    $show->trailer_url = $request->trailer_url ?? null;
+    $show->expiration  = $request->expiration ?? null;
 
-      Session::flash('success', "The <strong>{$show->type}</strong> show <strong>{$show->name}</strong> has been updated successfully!");
+    $request->has('cover') ? $show->cover =  $request->cover->store('shows', 'public') : null;
+
+    $show->save();
+
+    Session::flash('success', "The <strong>{$show->type}</strong> show <strong>{$show->name}</strong> has been updated successfully!");
+
+    // Log created event
+    Log::info(Auth::user()->fullname . ' edited Show ' . $show->name . ' using admin');
+
+    return redirect()->route('admin.shows.show', $show);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Show  $show
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Show $show)
+  {
+    // Log created event
+    Log::info(Auth::user()->fullname . ' deleted Show ' . $show->name . ' using admin');
+
+    $temp = $show;
+
+    $show->delete();
+
+    Session::flash('success', 'The ' . $temp->type . ' show ' . $temp->name . ' was successfully deleted.');
+
+    return redirect()->route('admin.shows.index');
+  }
+
+  public function delete(Show $show)
+  {
+    // How many events this show is part of, past and future
+    $showings = \App\Event::where('show_id', $show->id)->count();
+
+    // Check if there are any events have this show
+
+    if ($showings == 0) {
 
       // Log created event
-      Log::info(Auth::user()->fullname . ' edited Show ' . $show->name .' using admin');
+      Log::info(Auth::user()->fullname . ' deleted Show ' . $show->name . ' using admin');
+
+      $temp = $show;
+
+      $show->delete();
+
+      Session::flash('success', "The <strong>{$temp->type}</strong> show <strong>{$temp->name}</strong> was successfully deleted.");
+
+      return redirect()->route('admin.shows.index');
+    } else {
+
+      Session::flash('info', "Unable to delete <strong>{$show->name}</strong> because it is/will be featured in <strong>$showings or more events</strong>.");
 
       return redirect()->route('admin.shows.show', $show);
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Show  $show
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Show $show)
-    {
-        // Log created event
-        Log::info(Auth::user()->fullname . ' deleted Show ' . $show->name .' using admin');
-
-        $temp = $show;
-
-        $show->delete();
-
-        Session::flash('success', 'The '.$temp->type.' show '.$temp->name.' was successfully deleted.');
-
-        return redirect()->route('admin.shows.index');
-    }
-
-    public function delete(Show $show)
-    {
-        // How many events this show is part of, past and future
-        $showings = \App\Event::where('show_id', $show->id)->count();
-
-        // Check if there are any events have this show
-
-        if ($showings == 0)
-        {
-
-          // Log created event
-          Log::info(Auth::user()->fullname . ' deleted Show ' . $show->name .' using admin');
-
-          $temp = $show;
-
-          $show->delete();
-
-          Session::flash('success', "The <strong>{$temp->type}</strong> show <strong>{$temp->name}</strong> was successfully deleted.");
-
-          return redirect()->route('admin.shows.index');
-
-        }
-        else
-        {
-
-          Session::flash('info', "Unable to delete <strong>{$show->name}</strong> because it is/will be featured in <strong>$showings or more events</strong>.");
-
-          return redirect()->route('admin.shows.show', $show);
-
-        }
-
-
-    }
-
+  }
 }
