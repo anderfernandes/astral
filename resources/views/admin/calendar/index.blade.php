e42868f18d4131d52f71d755f265890abc431bc4
anderfernandes <handerson171@gmail.com>
2023-03-31 11:28:05 -0500

    Allow sundays to be visible in single day view

--- resources/views/admin/calendar/index.blade.php
+++ resources/views/admin/calendar/index.blade.php
@@ -8,99 +8,101 @@
 
 @section('content')
 
-  <div class="ui black icon buttons">
-    <div onclick="$('#admin-calendar').fullCalendar('prev')" class="ui button"><i class="left chevron icon"></i></div>
-    <div class="ui change date button"><i class="calendar alternate icon"></i></div>
-    <div onclick="$('#admin-calendar').fullCalendar('next')" class="ui button"><i class="right chevron icon"></i></div>
+<div class="ui black icon buttons">
+  <div onclick="$('#admin-calendar').fullCalendar('prev')" class="ui button"><i class="left chevron icon"></i></div>
+  <div class="ui change date button"><i class="calendar alternate icon"></i></div>
+  <div onclick="$('#admin-calendar').fullCalendar('next')" class="ui button"><i class="right chevron icon"></i></div>
+</div>
+
+@if (str_contains(Auth::user()->role->permissions['calendar'], "C"))
+<a class="ui black button" href="{{ route('admin.events.create') }}">
+  <i class="calendar plus icon"></i> Create Event
+</a>
+@endif
+
+@if (str_contains(Auth::user()->role->permissions['sales'], "C"))
+<a href="{{ @route('admin.sales.index') }}" class="ui black button">
+  <i class="icons"><i class="dollar icon"></i><i class="corner plus inverted icon"></i></i>
+  Create Sale
+</a>
+@endif
+
+<div class="ui right floated black icon button" onclick="$('#event-colors').modal('toggle')">
+  <i class="help circle icon"></i>
+</div>
+
+<div class="ui right floated black icon button" onclick="window.print()">
+  <i class="print icon"></i>
+</div>
+
+<div class="ui right floated secondary floating dropdown button" id="view">
+  <i class="eye icon"></i>
+  <span class="text">
+    @if ($request->view == "agendaDay")
+    Single Day
+    @elseif ($request->view == "agendaWeek")
+    Week
+    @else
+    Month
+    @endif
+  </span>
+  <i class="dropdown icon"></i>
+  <div class="menu">
+    <div onclick="$('#admin-calendar').fullCalendar('changeView', 'agendaDay')" class="{{ $request->view == 'agendaDay' ? 'active' : null }} item">Single Day</div>
+    <div onclick="$('#admin-calendar').fullCalendar('changeView', 'agendaWeek')" class="{{ $request->view == 'agendaWeek' ? 'active' : null }} item">Week</div>
+    <div onclick="$('#admin-calendar').fullCalendar('changeView', 'month')" class="{{ $request->view == 'month' ? 'active' : null }} item">Month</div>
   </div>
+</div>
 
-  @if (str_contains(Auth::user()->role->permissions['calendar'], "C"))
-  <a class="ui black button" href="{{ route('admin.events.create') }}">
-    <i class="calendar plus icon"></i> Create Event
-  </a>
-  @endif
-
-  @if (str_contains(Auth::user()->role->permissions['sales'], "C"))
-  <a href="{{ @route('admin.sales.index') }}" class="ui black button">
-    <i class="icons"><i class="dollar icon"></i><i class="corner plus inverted icon"></i></i>
-    Create Sale
-  </a>
-  @endif
-
-  <div class="ui right floated black icon button" onclick="$('#event-colors').modal('toggle')">
-    <i class="help circle icon"></i>
+<div class="ui right floated secondary floating dropdown button">
+  <i class="calendar alternate outline icon"></i>
+  <span class="text">Events</span>
+  <i class="dropdown icon"></i>
+  <div class="menu">
+    <div onclick="toggleCalendar('events')" class="{{ $request->type == "events" ? "active" : "" }} item">Events</div>
+    <div onclick="toggleCalendar('sales')" class="{{ $request->type == "sales" ? "active" : "" }} item">Sales</div>
   </div>
+</div>
 
-  <div class="ui right floated black icon button" onclick="window.print()">
-    <i class="print icon"></i>
-  </div>
-
-  <div class="ui right floated secondary floating dropdown button" id="view">
-    <i class="eye icon"></i>
-    <span class="text">
-      @if ($request->view == "agendaDay")
-        Single Day
-      @elseif ($request->view == "agendaWeek")
-        Week
-      @else
-        Month
-      @endif
-    </span>
-    <i class="dropdown icon"></i>
-    <div class="menu">
-      <div onclick="$('#admin-calendar').fullCalendar('changeView', 'agendaDay')" class="{{ $request->view == 'agendaDay' ? 'active' : null }} item">Single Day</div>
-      <div onclick="$('#admin-calendar').fullCalendar('changeView', 'agendaWeek')" class="{{ $request->view == 'agendaWeek' ? 'active' : null }} item">Week</div>
-      <div onclick="$('#admin-calendar').fullCalendar('changeView', 'month')" class="{{ $request->view == 'month' ? 'active' : null }} item">Month</div>
+@if (!isset($events) || $events->count() > 0)
+<br /><br />
+<div class="ui doubling stackable grid">
+  <div id="admin-calendar" style="min-width:100%; max-width:100%; padding-bottom: 2rem"></div>
+</div>
+@else
+<div class="ui info icon message">
+  <i class="info circle icon"></i>
+  <i class="close icon"></i>
+  <div class="content">
+    <div class="header">
+      No events!
     </div>
+    <p>It looks like there are no events in the database.</p>
   </div>
+</div>
+@endif
 
-  <div class="ui right floated secondary floating dropdown button">
-    <i class="calendar alternate outline icon"></i>
-    <span class="text">Events</span>
-    <i class="dropdown icon"></i>
-    <div class="menu">
-      <div onclick="toggleCalendar('events')" class="{{ $request->type == "events" ? "active" : "" }} item">Events</div>
-      <div onclick="toggleCalendar('sales')" class="{{ $request->type == "sales" ? "active" : "" }} item">Sales</div>
-    </div>
-  </div>
+@include('admin.partial.events._create')
 
-  @if (!isset($events) || $events->count() > 0)
-    <br /><br />
-    <div class="ui doubling stackable grid">
-      <div id="admin-calendar" style="min-width:100%; max-width:100%; padding-bottom: 2rem"></div>
-    </div>
-  @else
-    <div class="ui info icon message">
-      <i class="info circle icon"></i>
-      <i class="close icon"></i>
-      <div class="content">
-        <div class="header">
-          No events!
-        </div>
-        <p>It looks like there are no events in the database.</p>
-      </div>
-    </div>
-  @endif
+@include('admin.partial.help._event-colors')
 
-  @include('admin.partial.events._create')
+<div class="ui fullscreen modal" id="details"></div>
 
-  @include('admin.partial.help._event-colors')
+@include('admin.calendar._fetch-sales')
+@include('admin.calendar._fetch-events')
 
-  <div class="ui fullscreen modal" id="details"></div>
-
-  @include('admin.calendar._fetch-sales')
-  @include('admin.calendar._fetch-events')
-
-  <script>
-
+<script>
   function loadCalendar() {
     $('#admin-calendar').fullCalendar({
+      views: {
+        week: {
+          hiddenDays: [0],
+        },
+      },
       header: false,
-      views: null,
       defaultView: '{{ $request->view ?? 'agendaWeek' }}',
       defaultDate: moment('{{ $request->date ?? today() }}').format('YYYY-MM-DD'),
       contentHeight: 'auto',
-      hiddenDays: [0],
       navLinks: true,
       displayEventTime: false,
       navLinkDayClick: function(date, jsEvent) {
@@ -113,7 +115,7 @@
       eventLimit: true,
       minTime: '08:00:00',
       titleFormat: 'dddd, MMMM D, YYYY',
-      events: '{{ $request->type == "sales" ? '/api/calendar/sales' : '/api/calendar/events' }}',
+      events: '{{ $request->type == "sales" ? ' / api / calendar / sales ' : ' / api / calendar / events ' }}',
       loading: function(isLoading, view) {
         if (!isLoading) setTitle()
       },
@@ -130,9 +132,9 @@
 
   function setTitle() {
     var view = $('#admin-calendar').fullCalendar('getView')
-    var title = (view.name == 'month')
-      ? moment(view.intervalStart).format('MMMM YYYY')
-      : view.title
+    var title = (view.name == 'month') ?
+      moment(view.intervalStart).format('MMMM YYYY') :
+      view.title
     $('.header.active.item.hide-on-mobile').html(`<i class="calendar alternate icon"></i> Calendar | <strong>${title}</strong>`)
     document.title = ` Calendar | ${title}`
   }
@@ -151,21 +153,24 @@
     $('#admin-calendar').fullCalendar('addEventSource', `/api/calendar/${type}`)
   }
 
-  setInterval(() => { $('#admin-calendar').fullCalendar('refetchEvents') }, 15000)
+  setInterval(() => {
+    $('#admin-calendar').fullCalendar('refetchEvents')
+  }, 15000)
 
   $('.ui.change.date.button').flatpickr({
     onChange: function(selectedDates, dateStr, instance) {
       $('#admin-calendar').fullCalendar('gotoDate', dateStr)
     }
   })
-
 </script>
 
-  <style>
-    @media print {
-      .ui.button, .ui.label {
-        display: none !important;
-      }
-  </style>
+<style>
+  @media print {
+
+    .ui.button,
+    .ui.label {
+      display: none !important;
+    }
+</style>
 
 @endsection
