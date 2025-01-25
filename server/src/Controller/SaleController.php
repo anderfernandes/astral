6d6ac4b73d208a2713186cd5843f0b4ba570c011
anderfernandes <handerson171@gmail.com>
2025-01-24 23:01:53 -0600

    feat: require login for sales

--- server/src/Controller/SaleController.php
+++ server/src/Controller/SaleController.php
@@ -14,6 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class SaleController extends AbstractController
