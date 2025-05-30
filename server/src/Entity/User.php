ea0614e634343deae3804992b0daa4845af80911
anderfernandes <handerson171@gmail.com>
2025-05-29 21:41:01 -0500

    fixed testing configuration, deprecations

--- server/src/Entity/User.php
+++ server/src/Entity/User.php
@@ -187,6 +187,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see UserInterface
      */
+    #[\Deprecated]
     public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
