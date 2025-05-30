0da3fd8c93e5a15b83c1738ad644c3632b9b8766
anderfernandes <handerson171@gmail.com>
2025-05-29 20:15:58 -0500

    updating receipes, testing framework

--- server/tests/bootstrap.php
+++ server/tests/bootstrap.php
@@ -4,40 +4,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
-
-/*passthru(
-    sprintf(
-        'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
-        $_ENV['APP_ENV'],
-        __DIR__
-    )
-);
-
-passthru(
-    sprintf(
-        'APP_ENV=%s php "%s/../bin/console" doctrine:database:drop --force',
-        $_ENV['APP_ENV'],
-        __DIR__
-    )
-);
-
-passthru(
-    sprintf(
-        'APP_ENV=%s php "%s/../bin/console" doctrine:database:create',
-        $_ENV['APP_ENV'],
-        __DIR__
-    )
-);
-
-passthru(
-    sprintf(
-        'APP_ENV=%s php "%s/../bin/console" doctrine:schema:create',
-        $_ENV['APP_ENV'],
-        __DIR__
-    )
-);*/
