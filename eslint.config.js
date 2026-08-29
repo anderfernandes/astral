5f268f1026af17c6d0b11f993bcbc3c87872b943
anderfernandes <handerson171@gmail.com>
2026-08-29 12:54:51 -0500

    tslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,25 @@
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import pluginRouter from "@tanstack/eslint-plugin-router";
-import solid from "eslint-plugin-solid";
+import solid from "eslint-plugin-solid/config/v2";
+import tseslint from "typescript-eslint";
 
 export default [
-  ...pluginQuery.configs["flat/recommended-strict"],
-  ...pluginRouter.configs["flat/recommended"],
-  // Any other config...
+  pluginQuery.configs["flat/recommended-strict"],
+  pluginRouter.configs["flat/recommended"],
   solid,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  },
 ];
