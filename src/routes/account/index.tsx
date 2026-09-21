7404ea6ff38eaf651c8a10f733e2f1f34217cec5
anderfernandes <handerson171@gmail.com>
2026-09-20 22:14:28 -0500

    reset repo to receive solid 2 codebase

--- src/routes/account/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/solid-router'
-
-export const Route = createFileRoute('/account/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(account)/account/"!</div>
-}
--- tests/hasher.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-import { createHash, verifyHash } from "~utils/index.server";
-
-test("1: hasher hashers and verifies", async () => {
-  const hash = await createHash("MyTestP@ssw0rd123");
-  expect(await verifyHash("MyTestP@ssw0rd123", hash));
-});
